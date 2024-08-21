9900cc76adc5e088b1768c1b29600b68d1c3c726
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-08-20 21:28:14 -0300

    Add PlayerType enum

--- Game/CellType.cs
+++ Game/CellType.cs
@@ -3,7 +3,9 @@ namespace Game;
 public enum CellType
 {
     Empty = 0,
+
     Snake = 1,
+
     Food = 2,
 
     Outside = 69,
---
+++ Game/PlayerType.cs
@@ -0,0 +1,12 @@
+namespace Game;
+
+public enum PlayerType
+{
+    Human = 0,
+
+    DummyIfElse = 1,
+
+    SmartIfElse = 2,
+
+    NeuralNetwork = 3,
+}
